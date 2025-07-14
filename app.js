dbfd01f351f501495e2a7fd4edaee32948d99348
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-07-14 20:01:06 +0700

    new file kontak dan memanggil kontak dari app

--- app.js
+++ app.js
@@ -1,81 +1,12 @@
-const fs = require ('fs');
-
-// console.log(fs);
-
-// try {
-    
-//     fs.writeFileSync('data/test.txt', 'Hello, World!');
-// } catch (e) {
-//     console.log(e);
-// }
-
-
-//asychornus
-
-// fs.writeFile('data/test.txt', 'Hello, kontol!', (err) => {
-//         console.log(err);
-
-// });
-
-// const data = fs.readFileSync('data/test.txt', 'utf8')
-// console.log(data);
-
-// fs.readFileSync('data/test.txt', 'utf8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-// });
-
-const readLine = require('readline');
-const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const dirPath = './data';
-if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-}
-
-const filePath = './data/contact.json';
-if (!fs.existsSync(filePath)) { 
-    fs.writeFileSync(filePath,'[]','utf8')
-}
-
-
-const tulispertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (name) => {
-        resolve(name);
-    })
-})
-}
-
-// const pertanyaan2 = () => {
-//     return new Promise((resolve, reject) => {
-//         rl.question('What is your age? ', (age) => {
-//         resolve(age);
-//     })
-// })
-// }
+const contacts = require ('./contacts')
 
 const main = async () => {
 
-    const name = await tulispertanyaan('Masukan nama anda : ')
-    const age = await tulispertanyaan('Masukan umur anda : ')
+    const name = await contacts.tulispertanyaan('Masukan nama anda : ')
+    const age = await contacts.tulispertanyaan('Masukan umur anda : ')
 
 
-     const contact = { name, age };
-         const file = fs.readFileSync('data/contact.json','utf8');
-         const contacts =  JSON.parse(file);
-        
-         contacts.push(contact);
-         console.log(contacts);
-
-         fs.writeFileSync('data/contact.json', JSON.stringify(contacts))
-
-
-         console.log('Terimakasih sudah memasukam data pinjol');
-          rl.close() 
+    contacts.simpanKontak(name, age)     
 }
 
 main();
---
+++ contacts.js
@@ -0,0 +1,44 @@
+const fs = require ('fs');
+
+
+const readLine = require('readline');
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+
+const filePath = './data/contact.json';
+if (!fs.existsSync(filePath)) { 
+    fs.writeFileSync(filePath,'[]','utf8')
+}
+
+
+const tulispertanyaan = (pertanyaan) => {
+    return new Promise((resolve, reject) => {
+        rl.question(pertanyaan, (name) => {
+        resolve(name);
+    })
+})
+}
+
+const simpanKontak = (name, age) => {
+    const contact = { name, age };
+         const file = fs.readFileSync('data/contact.json','utf8');
+         const contacts =  JSON.parse(file);
+        
+         contacts.push(contact);
+         console.log(contacts);
+
+         fs.writeFileSync('data/contact.json', JSON.stringify(contacts))
+
+
+         console.log('Terimakasih sudah memasukam data pinjol');
+          rl.close() 
+}
+
+module.exports = { tulispertanyaan, simpanKontak }
