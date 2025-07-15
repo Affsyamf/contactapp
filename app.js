d0d417efe6d4cba56dabfc0027296e98bb5677ac
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-07-15 07:28:15 +0700

    yargs argv

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
+const { type } = require('os')
 const { argv } = require('process')
 const yargs = require ('yargs')
 //ambil argumen dari command line
 
-yargs.command (
-    'add',
-    'Menambahkan kontak baru : ',
-    () => {},
-    (argv) => {
-        console.log(argv.nama)
+yargs.command ({
+    command: 'add',
+    describe: 'Menambahkan kontak baru',
+    builder: {
+        nama: {
+            describe: 'Nama Lengkap',
+            demandOption: true,
+            type: 'string'
+        },
+        email: {
+            describe: 'Isi Email',
+            demandOption: true,
+            type: 'email'
+        }, 
+        nohp: {
+            describe: 'Isi nomor',
+            demandOption: true,
+            type: 'integer'
+        },
+    },
+    handler(argv) {
+        const contact = {
+            nama: argv.nama,
+            email: argv.email,
+            nohp: argv.nohp,
+        }
+        console.log(contact)
     }
-)
+})
 
 yargs.parse()
 
